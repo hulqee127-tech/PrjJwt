23775bca55f2e079474f09d2856d387ffce15e0b
hulqee127-tech <hulqee127@gmail.com>
2025-09-24 12:18:56 +0900

    first commit

---
+++ src/test/java/com/green/PrjJwtApplicationTests.java
@@ -0,0 +1,13 @@
+package com.green;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PrjJwtApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
